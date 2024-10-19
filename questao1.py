9cafdcbbcbd678b1bde32449ef6c14f3cc304bba
Larissa <larissafdds@gmail.com>
2024-10-19 17:48:23 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -13,6 +13,7 @@ beta.append(np.exp(-np.pi/2) + 0j)
 beta.append(-12345+28413j)
 
 # Funções
+                                    #axioma 1
 def soma(u, v):
     #Alterado:
     w = u + v #soma de vetores
@@ -30,10 +31,10 @@ for i in range(len(v)):
   for k in range(len(v) - i - 1):
     print(verifica_soma(v[i], v[k+i+1]) , ' => ', v[i], ' , ', v[k+i+1])
 
-
+                                    #axioma 6
 def produto(beta, u):
     #alterado:
-    modulo_beta = np.abs(beta)
+    modulo_beta = np.abs(np.sqrt(np.real(beta)**2 + np.imag(beta)**2)) #definição da operação produto
     w = modulo_beta * u #multiplicação de um vetor por um escalar complexo
     return w #retorna a multiplicação
 
@@ -51,7 +52,7 @@ for i in range(len(beta)):
     print(verifica_produto(beta[i], v[k]), ' => ', beta[i], ' , ', v[k])
     
     
-    
+                                    #axioma 4    
 # proponha o vetor nulo
     #alterado:
     nulo = np.zeros(3, dtype='complex128')
@@ -65,7 +66,7 @@ print('Testando vetor nulo:')
 for i in range(len(v)):
   print(verifica_nulo(v[i]), ' => ', v[i])
   
-  
+                                    #axioma 5
 # proponha o vetor inverso
 def inverso(v):
     w = -v
@@ -80,7 +81,7 @@ print('Testando vetor inverso:')
 for i in range(len(v)):
   print(verifica_inverso(v[i]), ' => ', v[i])
   
-  
+                                    #axioma 2
 def verifica_comutatividade(u, v):
     soma1 = soma(u, v) #usa a função soma para u + v
     soma2 = soma(v, u) #usa a função soma para v + u
@@ -92,7 +93,7 @@ for i in range(len(v)):
   for k in range(len(v) - i - 1):
     print(verifica_comutatividade(v[i], v[k+i+1]) , ' => ', v[i], ' , ', v[k+i+1]) # type: ignore
 
-
+                                    #axioma 3
 def verifica_associatividade(u, v, w):
     soma1 = soma(u, soma(v, w)) #Chama a função soma duas vezes. Primeiro somando v + w e na segunda o u + o resultado de v + w
     soma2 = soma(soma(u, v), w) #Chama a função soma duas vezes. Primeiro somando u + v e na segunda o resultado de (u + v) + w
@@ -105,7 +106,7 @@ for i in range(len(v)):
     for l in range(len(v) - i - k - 2):
       print(verifica_associatividade(v[i], v[k+i+1], v[k+i+l+2]) , ' => ', v[i], ' , ', v[k+i+1], ' , ', v[k+i+l+2])
    
-        
+                                    #axioma 7        
 def verifica_distributividade_1(beta, u, v):
     resultado1 = produto(beta, soma(u, v)) #Chama a função produto, passando como paramentro o beta e a função soma, somando u + v
     resultado2 = soma(produto(beta, u), produto(beta, v)) #Chama a função soma, passando como paramentro 2x a função produto, multiplicando o mesmo escalar com os vetores u e v, respectivamente
@@ -118,7 +119,7 @@ for i in range(len(beta)):
     for l in range(len(v) - k - 1):
       print(verifica_distributividade_1(beta[i], v[k], v[k+l+1]), ' => ', beta[i], ' , ', v[k], ' , ', v[k+l+1])
 
-
+                                    #axioma 8
 def verifica_distributividade_2(beta, gama, u):
     resultado1 = produto(beta + gama, u) #Chama a função produto com os paramentros beta + gama e o vetor u
     resultado2 = soma(produto(beta, u), produto(gama, u)) #Chama a função soma com os paramentros sendo 2x a função produto, que é chamada com os paramentros escalar1 e u, escalar2 e u
@@ -131,7 +132,7 @@ for i in range(len(beta)):
     for l in range(len(v)):
       print(verifica_distributividade_2(beta[i], beta[i+k+1], v[l]), ' => ', beta[i], ' , ', beta[i+k+1], ' , ', v[l])
 
-
+                                    #axioma 9
 def verifica_distributividade_3(beta, gama, u):
     resultado1 = produto(beta, produto(gama, u)) #chama a função produto com os paramentros escalar1 e a função produto, com os paramentros escalar2 e u
     resultado2 = produto(beta * gama, u) #chama a função produto com o paramentro beta*gama e u
@@ -144,7 +145,7 @@ for i in range(len(beta)):
     for l in range(len(v)):
       print(verifica_distributividade_3(beta[i], beta[i+k+1], v[l]), ' => ', beta[i], ' , ', beta[i+k+1], ' , ', v[l])
 
-
+                                    #axioma 10
 def verifica_escalar_unitario(u):
     resultado = produto(1, u) #chama a função produto com os paramentros 1 e u
     return np.array_equal(resultado, u) #analisa se os resultados são iguais, se sim, true, caso não, false
